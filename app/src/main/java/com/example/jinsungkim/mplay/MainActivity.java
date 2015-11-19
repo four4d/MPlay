68ecb05071d5ef1f5f48cc8723560f036d4a26d3
JinSung Kim <four4d@naver.com>
2015-11-19 13:24:38 +0900

    musicplayer

--- app/src/main/java/com/example/jinsungkim/mplay/MainActivity.java
+++ app/src/main/java/com/example/jinsungkim/mplay/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.jinsungkim.mplay;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btStart;
+    Button btStop;
+    MediaPlayer mMplayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btStart = (Button) findViewById(R.id.bt_start);
+        btStop = (Button) findViewById(R.id.bt_stop);
+
+        mMplayer = new MediaPlayer();
+
+        // Prepare mp3 file for MediaPlayer
+        try {
+            AssetFileDescriptor afd = getResources().openRawResourceFd(R.raw.boysandgirls);
+            mMplayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        } catch(Exception e) {
+            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        btStart.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View view) {
+                                           try {
+                                               mMplayer.prepare();
+                                               mMplayer.start();
+                                           } catch (IOException e) {
+                                               Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                                           }
+                                       }
+                                   }
+        );
+
+        // Music Stop
+        btStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMplayer.stop();
+            }
+        });
+
+
+
     }
 }
